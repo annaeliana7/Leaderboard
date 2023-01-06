4404d1f6f25e5c7199cca79224a61cf1fef4a7f9
Anna Petras <apetras2017@gmail.com>
2023-01-06 10:25:11 +0100

    Fix linter errors

--- src/module/pull.js
+++ src/module/pull.js
@@ -1,8 +1,7 @@
-import display from "./display.js";
+import display from './display.js';
 
 const pullScore = async () => {
-  const url =
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/gameId777/scores/';
+  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/gameId777/scores/';
   const response = await fetch(url);
   const json = await response.json();
   const scores = json.result;
