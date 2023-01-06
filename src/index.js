56ecbe349623b9567017d3075e3d5e8cf9f66324
Anna Petras <apetras2017@gmail.com>
2023-01-06 10:15:18 +0100

    Add eventListeners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+import pullScore from './module/pull.js';
+import postScore from './module/post.js';
+
+const form = document.querySelector('.submit-btn');
+const refresh = document.querySelector('.btn-refresh');
+
+form.addEventListener('submit', postScore);
+refresh.addEventListener('click', pullScore);
